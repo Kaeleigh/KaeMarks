07422d85efc2e16cc1e9ff581ec19fcae5e0379c
[] <awh938@gmail.com>
2018-04-12 13:17:07 -0400

    fixing mailgun email

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -1,5 +1,6 @@
 class IncomingController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
+  skip_before_action :authenticate_user!, only: [:create]
 
   def create
     user = User.find_by(email: params[:sender])
@@ -8,7 +9,7 @@ class IncomingController < ApplicationController
       head 404
     else
       topic = Topic.find_or_create_by(user: user, title: params[:subject])
-      topic.bookmark.create(url: params["body-plain"])
+      topic.bookmarks.create(url: params["body-plain"])
       head 200
     end
   end
