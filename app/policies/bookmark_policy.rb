e5a7f0af3c7690721430c66f48ee26bf278bdd92
[] <awh938@gmail.com>
2018-04-19 13:09:52 -0400

    policy fixed

--- app/policies/bookmark_policy.rb
+++ app/policies/bookmark_policy.rb
@@ -1,2 +1,15 @@
 class BookmarkPolicy < ApplicationPolicy
+
+  def create?
+    user.present?
+  end
+
+  def update?
+    user.present? && (record.user == user)
+  end
+
+  def destroy?
+    user.present? && (record.user == user)
+  end
+  
 end
